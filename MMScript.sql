e13e1988a0cd98bf3aa4f5611028f593e0498a8d
Sourav Mahato <95037368+somahato@users.noreply.github.com>
2023-09-15 14:02:45 +0530

    Delete MMScript.sql

--- MMScript.sql
+++
@@ -1,8 +0,0 @@
-Declare @Start DateTime
-Declare @EndTime DateTime
-Declare @Object uniqueidentifier
-
-Set @Start = GETUTCDATE()
-Set @EndTime = DATEADD(HH,1,GETUTCDATE())
-Set @Object = (Select BaseManagedEntityId From BaseManagedEntity Where FullName like 'Microsoft.Windows.Computer:ServerNamewithFQDN')
-exec dbo.p_MaintenanceModeStart @BaseManagedEntityId=@Object,@StartTime=@Start,@ScheduledEndTime=@EndTime,@ReasonCode=1,@Comments=N'Using SQL script',@User=N'LAB\sourav',@Recursive=1
